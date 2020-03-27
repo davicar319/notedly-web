4169db2dd3932c24c7d80e9e975d606e46534956
Dave Carlson <dave.carlson@tr.com>
2020-03-27 14:43:20 -0500

    Adding in the shell of each application page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import Routes
+import Pages from '/pages';
+
+
 const App = () => {
   return (
     <div>
-      <h1>Hello Notedly</h1>
-      <p>Welcome to the Notedly application.</p>
+      <Pages/>
     </div>
   );
 };
