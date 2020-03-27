22a52c440a7d4fdf27b59ab6be45c63164ad33d4
Dave Carlson <dave.carlson@tr.com>
2020-03-27 15:53:37 -0500

    Adding a button to our homepage.  Creating a component for our button as well.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../compontents/Button';
 
 const Home = () => {
   return (
     <div>
       <p>This is the Home Page.</p>
+      <Button>Click Me!</Button>
     </div>
   );
 };
