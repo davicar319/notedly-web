04f48e01e45c70b8254b234610bccc573bbd0f8c
Dave Carlson <dave.carlson@tr.com>
2020-03-27 14:47:04 -0500

    Adding links from the home page to the other pages.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,20 @@
 import React  from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Notedly</h1>
       <p>This is the home page.</p>
+      { /* add a list of links */ }
+      <ul>
+        <li>
+          <Link to={"/mynotes"}>Ny Notes</Link>
+        </li>
+        <li>
+          <Link to={"/favorites"}>Favorites</Link>
+        </li>
+      </ul>
     </div>
   );
 };
