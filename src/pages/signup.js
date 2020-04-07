c83d9d43a3d6c00656933b0770bdf56ca2c0726a
Dave_C <dave@ace.programming.com>
2020-04-07 15:12:27 -0500

    Added mutation for doing signup.

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 import styled from 'styled-components';
 
 import Button from '../components/Button';
+
+const SIGNUP_USER = gql`
+    mutation signUp($email: String!, $username: String!, $password: String!) {
+        signUp(email: $email, username: $username, password: $password)
+    }
+`;
 
 const Wrapper = styled.div`
   border: 1px solid #f5f4f0;
@@ -24,28 +31,53 @@ const Form = styled.form`
 
 //Include the props passed to the component for later use.
 const SignUp = props => {
+  // set the default state of the form.
+  const [values, setValues] = useState();
+
+  // update the state when a user types in the form.
+  const onChange = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
   useEffect(() => {
     //Update the document title.
     document.title = 'Sign Up -- Notedly';
   });
 
+  // add the mutation hook
+  const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+    onCompleted: data => {
+      // console.log the JSON Web Token when the mutation is complete.
+      console.log(data.signUp);
+    }
+  });
+
   return (
-    <div>
-      <Wrapper>
-        <Form>
-          <label htmlFor={'username'}>Username:</label>
-          <input required={'true'} type={'text'} id={'username'} name={'username'}
-                 placeholder={'username'}/>
-          <label htmlFor={'email'}>Email: </label>
-          <input required={'true'} type={'email'} id={'email'} name={'email'}
-                 placeholder={'Email'}/>
-          <label htmlFor={'password'}>Password:</label>
-          <input required={'true'} type={'password'} id={'password'}
-                 name={'password'} placeholder={'Password'}/>
-          <Button type={'submit'}>Submit</Button>
-        </Form>
+    <Wrapper>
+      <h2>Sign Up</h2>
+      <Form onSubmit={event => {
+        event.preventDefault();
+        signUp({
+          variables: {
+            ...values
+          }
+        });
+      }}>
+        <label htmlFor={'username'}>Username:</label>
+        <input required={true} type={'text'} id={'username'} name={'username'}
+               placeholder={'username'} onChange={onChange}/>
+        <label htmlFor={'email'}>Email: </label>
+        <input required={true} type={'email'} id={'email'} name={'email'}
+               placeholder={'Email'} onChange={onChange}/>
+        <label htmlFor={'password'}>Password:</label>
+        <input required={true} type={'password'} id={'password'}
+               name={'password'} placeholder={'Password'} onChange={onChange}/>
+        <Button type={'submit'}>Submit</Button>
+      </Form>
       </Wrapper>
-    </div>
   );
 };
 
