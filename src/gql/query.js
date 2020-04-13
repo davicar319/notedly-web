06831effbf33d835a18dd6120e51e218c5ec0faa
Dave_C <dave@ace.programming.com>
2020-04-13 14:00:16 -0500

    Ability to fetch favorites.

--- src/gql/query.js
+++ src/gql/query.js
@@ -56,10 +56,30 @@ const GET_MY_NOTES = gql`
     }
 `;
 
+const GET_MY_FAVORITES = gql`
+    query me {
+        me {
+            id
+            username
+            favorites {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`;
+
 const IS_LOGGED_IN = gql`
     {
         isLoggedIn @client
     }
 `;
 
-export { GET_NOTE, GET_NOTES, GET_MY_NOTES, IS_LOGGED_IN };
+export { GET_NOTE, GET_NOTES, GET_MY_NOTES, GET_MY_FAVORITES, IS_LOGGED_IN };
--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import NoteFeed from '../components/NoteFeed';
+import { GET_MY_FAVORITES } from '../gql/query';
+
 
 const Favorites = () => {
   useEffect(() => {
@@ -6,11 +10,15 @@ const Favorites = () => {
     document.title = 'Favorites -- Notedly';
   });
 
-  return (
-    <div>
-      <p>These are my favorites.</p>
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_MY_FAVORITES);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  if (data.me.favorites.length !== 0) {
+    return <NoteFeed notes={data.me.favorites}/>;
+  } else {
+    return <p>No favorites yet.</p>;
+  }
 };
 
 export default Favorites;
