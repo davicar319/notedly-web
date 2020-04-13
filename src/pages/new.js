4cd4a5f41125fa8be0859c7a65331ca34792729d
Dave_C <dave@ace.programming.com>
2020-04-13 14:02:17 -0500

    Make sure we refresh MyNotes when data is updated.

--- src/pages/new.js
+++ src/pages/new.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { gql, useMutation } from '@apollo/client';
 
 import NoteForm from '../../final/components/NoteForm';
-import { GET_NOTES } from '../gql/query';
+import { GET_MY_NOTES, GET_NOTES } from '../gql/query';
 
 const NEW_NOTE = gql`
     mutation newNote ($content: String!) {
@@ -30,7 +30,7 @@ const NewNote = props => {
 
   const [data, { loading, error }] = useMutation(NEW_NOTE, {
     // refetch the GET_NOTES query to update the cache
-    refetchQueries: [{ query: GET_NOTES }],
+    refetchQueries: [{ query: GET_NOTES }, { query: GET_MY_NOTES }],
     onCompleted: data => {
       // when complete, redirect user to the note page.
       props.history.push(`note/${data.newNote.id}`);
