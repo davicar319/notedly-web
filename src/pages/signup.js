f2846dddfdc848b60a9d364fd7d5dbd908408a6b
Dave_C <dave@ace.programming.com>
2020-04-13 10:29:15 -0500

    Refactoring signup to use the common user form.

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { gql, useApolloClient, useMutation } from '@apollo/client';
-import styled from 'styled-components';
 
-import Button from '../components/Button';
+import UserForm from '../components/UserForm';
 
 const SIGNUP_USER = gql`
     mutation signUp($email: String!, $username: String!, $password: String!) {
@@ -10,38 +9,8 @@ const SIGNUP_USER = gql`
     }
 `;
 
-const Wrapper = styled.div`
-  border: 1px solid #f5f4f0;
-  max-width: 500px;
-  padding: 1em;
-  margin 0 auto;
-`;
-
-const Form = styled.form`
-  label, input {
-    display: block;
-    line-height: 2em;
-  }
-  input {
-    width: 100%;
-    margin-bottom: 1em;
-  }
-
-`;
-
 //Include the props passed to the component for later use.
 const SignUp = props => {
-  // set the default state of the form.
-  const [values, setValues] = useState();
-
-  // update the state when a user types in the form.
-  const onChange = event => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value
-    });
-  };
-
   useEffect(() => {
     //Update the document title.
     document.title = 'Sign Up -- Notedly';
@@ -62,28 +31,13 @@ const SignUp = props => {
   });
 
   return (
-    <Wrapper>
-      <h2>Sign Up</h2>
-      <Form onSubmit={event => {
-        event.preventDefault();
-        signUp({
-          variables: {
-            ...values
-          }
-        });
-      }}>
-        <label htmlFor={'username'}>Username:</label>
-        <input required={true} type={'text'} id={'username'} name={'username'}
-               placeholder={'username'} onChange={onChange}/>
-        <label htmlFor={'email'}>Email: </label>
-        <input required={true} type={'email'} id={'email'} name={'email'}
-               placeholder={'Email'} onChange={onChange}/>
-        <label htmlFor={'password'}>Password:</label>
-        <input required={true} type={'password'} id={'password'}
-               name={'password'} placeholder={'Password'} onChange={onChange}/>
-        <Button type={'submit'}>Submit</Button>
-      </Form>
-      </Wrapper>
+    <React.Fragment>
+      <UserForm action={signUp} formType={'signup'}/>
+      {/* if the data is loading, display a loading message. */}
+      {loading && <p>Loading...</p>}
+      {/* if there is an error, display an error message */}
+      {error && <p>Error creating an account.</p>}
+    </React.Fragment>
   );
 };
 
